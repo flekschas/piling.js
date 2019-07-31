bfd911569df5d705a6a6916675e3e6e1d4c0f99c
Fritz Lekschas <service@lekschas.de>
2019-07-31 18:16:45 -0400

    Add a second image

--- examples/index.js
+++ examples/index.js
@@ -9,5 +9,9 @@ pileMe.set('items', [
     id: 'test',
     src:
       'http://pbs.twimg.com/profile_images/851447292120805376/y_RzZDR__normal.jpg'
+  },
+  {
+    id: 'test2',
+    src: 'https://ask-me-or-tell-me.s3.us-east-2.amazonaws.com/assaaa.png'
   }
 ]);
