3a434bdae350f262d439202005f8b554a8ec4a79
eeeeenchanted <597380714@qq.com>
2019-08-05 21:42:01 -0400

    add test images

--- examples/index.js
+++ examples/index.js
@@ -7,11 +7,47 @@ pileMe.set('renderer', imageRenderer);
 pileMe.set('items', [
   {
     id: 'test',
-    src:
-      'http://pbs.twimg.com/profile_images/851447292120805376/y_RzZDR__normal.jpg'
+    src: 'data/cat_0000.jpg'
   },
   {
     id: 'test2',
-    src: 'https://ask-me-or-tell-me.s3.us-east-2.amazonaws.com/assaaa.png'
+    src: 'data/cat_0001.jpg'
+  },
+  {
+    id: 'test3',
+    src: 'data/cat_0002.jpg'
+  },
+  {
+    id: 'test4',
+    src: 'data/cat_0003.jpg'
+  },
+  {
+    id: 'test5',
+    src: 'data/cat_0004.jpg'
+  },
+  {
+    id: 'test6',
+    src: 'data/cat_0005.jpg'
+  },
+  {
+    id: 'test7',
+    src: 'data/cat_0006.jpg'
+  },
+  {
+    id: 'test8',
+    src: 'data/cat_0007.jpg'
+  },
+  {
+    id: 'test9',
+    src: 'data/cat_0008.jpg'
+  },
+  {
+    id: 'test10',
+    src: 'data/cat_0009.jpg'
+  },
+  {
+    id: 'test11',
+    src: 'data/cat_0010.jpg'
   }
 ]);
+pileMe.set('grid', [10, 10]);
