684140265c62edd0e722df34367c8e2cf09f473a
eeeeenchanted <597380714@qq.com>
2019-08-05 21:42:13 -0400

    add grid layout

---
+++ src/grid.js
@@ -0,0 +1,34 @@
+const creatGrid = (canvas, [cols, rows, rowHeight, cellRatio]) => {
+  const { width, height } = canvas.getBoundingClientRect();
+
+  const myColNum = cols;
+  let myRowNum;
+  const myColWidth = width / cols;
+  let myRowHeight;
+  let myCellRatio; // height = ratio * width
+
+  if (!+rows && !+rowHeight && !+cellRatio) {
+    myCellRatio = 1;
+    myRowHeight = myColWidth;
+  } else if (+cellRatio) {
+    myCellRatio = cellRatio;
+    myRowHeight = myCellRatio * myColWidth;
+  } else if (+rowHeight) {
+    if (!myRowHeight) myRowHeight = rowHeight;
+  } else if (+rows) {
+    if (!myRowHeight) {
+      myRowNum = rows;
+      myRowHeight = height / rows;
+    }
+  }
+
+  return {
+    myColNum,
+    myRowNum,
+    myColWidth,
+    myRowHeight,
+    myCellRatio
+  };
+};
+
+export default creatGrid;
