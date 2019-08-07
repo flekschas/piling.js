0596843a4508506799fc12fada001884449d106b
eeeeenchanted <597380714@qq.com>
2019-08-06 21:42:24 -0400

    detect pile collision

---
+++ src/pileManager.js
@@ -0,0 +1,34 @@
+const hitTestRectangle = (r1, r2) => {
+  let hit;
+
+  hit = false;
+
+  r1.centerX = r1.x + r1.width / 2;
+  r1.centerY = r1.y + r1.height / 2;
+  r2.centerX = r2.x + r2.width / 2;
+  r2.centerY = r2.y + r2.height / 2;
+
+  r1.halfWidth = r1.width / 2;
+  r1.halfHeight = r1.height / 2;
+  r2.halfWidth = r2.width / 2;
+  r2.halfHeight = r2.height / 2;
+
+  const vx = r1.centerX - r2.centerX;
+  const vy = r1.centerY - r2.centerY;
+
+  const combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+  const combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+  if (Math.abs(vx) < combinedHalfWidths) {
+    if (Math.abs(vy) < combinedHalfHeights) {
+      hit = true;
+    } else {
+      hit = false;
+    }
+  } else {
+    hit = false;
+  }
+  return hit;
+};
+
+export default hitTestRectangle;
