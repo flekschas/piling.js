f761033ebd1f73e06bc8c9cd6da1afa8c7c44f8e
eeeeenchanted <597380714@qq.com>
2019-08-15 22:38:16 -0400

    add pile reducer and actions

--- src/store.js
+++ src/store.js
@@ -60,11 +60,101 @@ export const setGrid = newGrid => ({
   payload: { grid: newGrid }
 });
 
+// reducer
+const piles = (previousState = [], action) => {
+  switch (action.type) {
+    case 'INIT_PILES': {
+      const itemIds = [];
+      for (let i = 0; i < action.payload.itemLength; i++) {
+        itemIds.push(i);
+      }
+      return itemIds.map(id => ({
+        items: [id],
+        x: 0,
+        y: 0
+      }));
+    }
+    case 'MERGE_PILES': {
+      const newState = [...previousState];
+
+      if (action.payload.isDropped) {
+        const source = action.payload.pileIds[0];
+        const target = action.payload.pileIds[1];
+
+        newState[target] = {
+          ...newState[target],
+          items: [...newState[target].items]
+        };
+
+        newState[target].items.push(...newState[source].items);
+        newState[source] = {};
+      } else {
+        const target = Math.min(...action.payload.pileIds);
+        const sourcePileIds = action.payload.pileIds.filter(
+          id => id !== target
+        );
+
+        let centerX = 0;
+        let centerY = 0;
+        action.payload.pileIds.forEach(id => {
+          centerX += newState[id].x;
+          centerY += newState[id].y;
+        });
+        centerX /= action.payload.pileIds.length;
+        centerY /= action.payload.pileIds.length;
+
+        newState[target] = {
+          ...newState[target],
+          items: [...newState[target].items],
+          x: centerX,
+          y: centerY
+        };
+
+        sourcePileIds.forEach(id => {
+          newState[target].items.push(...newState[id].items);
+          newState[id] = {};
+        });
+      }
+      return newState;
+    }
+    case 'MOVE_PILES': {
+      const newState = [...previousState];
+      action.payload.movingPiles.forEach(({ id, x, y }) => {
+        newState[id] = {
+          ...newState[id],
+          x,
+          y
+        };
+      });
+      return newState;
+    }
+    default:
+      return previousState;
+  }
+};
+
+// action
+export const initPiles = itemLength => ({
+  type: 'INIT_PILES',
+  payload: { itemLength }
+});
+
+export const mergePiles = (pileIds, isDropped) => ({
+  type: 'MERGE_PILES',
+  payload: { pileIds, isDropped }
+});
+
+export const movePiles = movingPiles => ({
+  type: 'MOVE_PILES',
+  payload: { movingPiles }
+});
+
 const createStore = () => {
   const appReducer = combineReducers({
     // This defines what is on our store
     itemRenderer,
     items,
+    piles,
     orderer,
     grid
   });
