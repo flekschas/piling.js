bcd1b6d67cfa968d083aa3739d6fee6bf91e7417
eeeeenchanted <597380714@qq.com>
2019-08-22 22:15:46 -0400

    re-organize pile class

--- src/pile.js
+++ src/pile.js
@@ -1,18 +1,36 @@
 import * as PIXI from 'pixi.js';
 
-export const ANCHOR = [0, 0];
+// export const ANCHOR = [0, 0];
 
-const createPile = (item, renderRaf, index, pubSub) => {
-  const drawBorder = (pile, border) => {
-    const rect = pile.getChildAt(1).getBounds();
+const createPile = (item, renderRaf, id, pubSub) => {
+  const itemIds = new Map();
+  const newItemIds = new Map();
+  const pileGraphics = new PIXI.Graphics();
+  const itemContainer = new PIXI.Container();
+  const borderContainer = new PIXI.Container();
+  const hoverItemContainer = new PIXI.Container();
+  const bBox = {
+    minX: null,
+    minY: null,
+    maxX: null,
+    maxY: null,
+    pileId: id
+  };
+
+  const handleHoverItem = hoveredItem => {
+    console.log(hoveredItem.id);
+  };
+
+  const drawBorder = border => {
+    const rect = pileGraphics.getChildAt(1).getBounds();
 
     border.clear();
     border.lineStyle(2, 0xfeeb77, 1);
     border.drawRect(
       // eslint-disable-next-line no-use-before-define
-      calcBBox().minX - pile.x - 2,
+      calcBBox().minX - pileGraphics.x - 2,
       // eslint-disable-next-line no-use-before-define
-      calcBBox().minY - pile.y - 2,
+      calcBBox().minY - pileGraphics.y - 2,
       rect.width + 4,
       rect.height + 4
     );
@@ -20,127 +38,96 @@ const createPile = (item, renderRaf, index, pubSub) => {
     renderRaf();
   };
 
-  const onMouseDown = (pile, border) => () => {
-    pile.isMouseDown = true;
-    drawBorder(pile, border);
+  const onMouseDown = border => () => {
+    pileGraphics.isMouseDown = true;
+    drawBorder(border);
   };
 
-  const onMouseUp = (pile, border) => () => {
-    pile.isMouseDown = false;
-    if (pile.isHover) {
-      drawBorder(pile, border);
+  const onMouseUp = border => () => {
+    pileGraphics.isMouseDown = false;
+    if (pileGraphics.isHover) {
+      drawBorder(border);
     } else {
       border.clear();
     }
   };
 
-  const onMouseOver = (pile, border) => () => {
-    pile.isHover = true;
-    drawBorder(pile, border);
+  let isSubscribe = false;
+
+  const onMouseOver = border => () => {
+    pileGraphics.isHover = true;
+    drawBorder(border);
+    // add pubSub subscription for hoverItem
+    if (!isSubscribe) {
+      pubSub.subscribe('hoverItem', handleHoverItem);
+      isSubscribe = true;
+    }
   };
 
-  const onMouseOut = (pile, border) => () => {
-    if (pile.isDragging) return;
-    pile.isHover = false;
+  const onMouseOut = border => () => {
+    if (pileGraphics.isDragging) return;
+    pileGraphics.isHover = false;
     border.clear();
     renderRaf();
+    // remove pubSub subscription for hoverItem
+    pubSub.unsubscribe('hoverItem');
   };
 
-  const initHover = (pile, borderContainer) => {
+  const initHover = () => {
     const border = new PIXI.Graphics();
     borderContainer.addChild(border);
 
-    pile
-      .on('pointerdown', onMouseDown(pile, border))
-      .on('pointerup', onMouseUp(pile, border))
-      .on('pointerupoutside', onMouseUp(pile, border))
-      .on('pointerover', onMouseOver(pile, border))
-      .on('pointerout', onMouseOut(pile, border));
+    pileGraphics
+      .on('pointerdown', onMouseDown(border))
+      .on('pointerup', onMouseUp(border))
+      .on('pointerupoutside', onMouseUp(border))
+      .on('pointerover', onMouseOver(border))
+      .on('pointerout', onMouseOut(border));
   };
 
-  const onDragStart = pile => event => {
+  const onDragStart = event => {
     // trigger active pile
-    pubSub.publish('dragPile', index);
+    pubSub.publish('dragPile', id);
 
     // first get the offset from the mouse position to the current pile.x and pile.y
     // And store it (draggingMouseOffset = [x, y])
-    pile.draggingMouseOffset = [
-      event.data.getLocalPosition(pile.parent).x - pile.x,
-      event.data.getLocalPosition(pile.parent).y - pile.y
+    pileGraphics.draggingMouseOffset = [
+      event.data.getLocalPosition(pileGraphics.parent).x - pileGraphics.x,
+      event.data.getLocalPosition(pileGraphics.parent).y - pileGraphics.y
     ];
-    pile.alpha = 1;
-    pile.isDragging = true;
+    pileGraphics.alpha = 1;
+    pileGraphics.isDragging = true;
     renderRaf();
   };
 
-  const onDragEnd = pile => () => {
-    if (!pile.isDragging) return;
-    pile.alpha = 1;
-    pile.isDragging = false;
-    pile.draggingMouseOffset = null;
+  const onDragEnd = () => {
+    if (!pileGraphics.isDragging) return;
+    pileGraphics.alpha = 1;
+    pileGraphics.isDragging = false;
+    pileGraphics.draggingMouseOffset = null;
     // trigger collision check
-    pubSub.publish('dropPile', index);
+    pubSub.publish('dropPile', id);
     renderRaf();
   };
 
-  const onDragMove = pile => event => {
-    if (pile.isDragging) {
-      const newPosition = event.data.getLocalPosition(pile.parent);
+  const onDragMove = event => {
+    if (pileGraphics.isDragging) {
+      const newPosition = event.data.getLocalPosition(pileGraphics.parent);
       // remove offset
-      pile.x = newPosition.x - pile.draggingMouseOffset[0];
-      pile.y = newPosition.y - pile.draggingMouseOffset[1];
-      pubSub.publish('highlightPile', index);
+      pileGraphics.x = newPosition.x - pileGraphics.draggingMouseOffset[0];
+      pileGraphics.y = newPosition.y - pileGraphics.draggingMouseOffset[1];
+      pubSub.publish('highlightPile', id);
 
       renderRaf();
     }
   };
 
-  const initDrag = pile => {
-    pile
-      .on('pointerdown', onDragStart(pile))
-      .on('pointerup', onDragEnd(pile))
-      .on('pointerupoutside', onDragEnd(pile))
-      .on('pointermove', onDragMove(pile));
-  };
-
-  const itemIds = new Map();
-  const newItemIds = new Map();
-
-  const initPile = () => {
-    const pile = new PIXI.Graphics();
-    const itemContainer = new PIXI.Container();
-    const borderContainer = new PIXI.Container();
-    pile.addChild(borderContainer);
-    pile.addChild(itemContainer);
-
-    pile.interactive = true;
-    pile.buttonMode = true;
-    pile.x = 0; // use the ANCHOR
-    pile.y = 0;
-    // Origin of the items coordinste system relative to the pile
-    item.anchor.set(0);
-    item.x = 2;
-    item.y = 2;
-
-    initHover(pile, borderContainer);
-    initDrag(pile);
-
-    itemContainer.addChild(item);
-
-    itemIds.set(index, item);
-
-    return pile;
-  };
-
-  const pileGraphics = initPile();
-  const id = index;
-
-  const bBox = {
-    minX: null,
-    minY: null,
-    maxX: null,
-    maxY: null,
-    pileId: id
+  const initDrag = () => {
+    pileGraphics
+      .on('pointerdown', onDragStart)
+      .on('pointerup', onDragEnd)
+      .on('pointerupoutside', onDragEnd)
+      .on('pointermove', onDragMove);
   };
 
   const setBBox = newBBox => {
@@ -152,10 +139,6 @@ const createPile = (item, renderRaf, index, pubSub) => {
 
   const calcBBox = () => {
     // compute bounding box
-    // const minX = pileGraphics.x;
-    // const minY = pileGraphics.y;
-    // const maxX = minX + pileGraphics.getChildAt(1).width;
-    // const maxY = minY + pileGraphics.getChildAt(1).height;
 
     let minX = Infinity;
     let minY = Infinity;
@@ -233,9 +216,34 @@ const createPile = (item, renderRaf, index, pubSub) => {
     }
   };
 
+  const init = () => {
+    pileGraphics.addChild(borderContainer);
+    pileGraphics.addChild(itemContainer);
+    pileGraphics.addChild(hoverItemContainer);
+
+    pileGraphics.interactive = true;
+    pileGraphics.buttonMode = true;
+    pileGraphics.x = 0;
+    pileGraphics.y = 0;
+    // Origin of the items coordinste system relative to the pile
+    item.anchor.set(0);
+    item.x = 2;
+    item.y = 2;
+
+    // initHoverItem();
+
+    initHover();
+    initDrag();
+
+    itemContainer.addChild(item);
+
+    itemIds.set(id, item);
+  };
+
+  init();
+
   return {
     drawBorder,
-    initPile,
     itemIds,
     newItemIds,
     pileGraphics,
