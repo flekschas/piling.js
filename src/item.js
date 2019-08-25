6f67b50f61f498e018981099d10af9188b83bedc
eeeeenchanted <597380714@qq.com>
2019-08-25 19:43:03 -0400

    edit item class

--- src/item.js
+++ src/item.js
@@ -1,16 +1,18 @@
 import * as PIXI from 'pixi.js';
 
-const createItem = (id, sprite, pubSub) => {
-  const clone = () => {
-    const clonedItem = new PIXI.Sprite(sprite.texture);
-    clonedItem.interactive = true;
-    clonedItem.x = sprite.x;
-    clonedItem.y = sprite.y;
-    clonedItem.width = sprite.width;
-    clonedItem.height = sprite.height;
-    clonedItem.angle = sprite.angle;
+const createItem = (id, texture, pubSub) => {
+  const sprite = new PIXI.Sprite(texture);
 
-    return clonedItem;
+  const cloneSprite = () => {
+    const clonedSprite = new PIXI.Sprite(texture);
+    clonedSprite.interactive = true;
+    clonedSprite.x = sprite.x;
+    clonedSprite.y = sprite.y;
+    clonedSprite.width = sprite.width;
+    clonedSprite.height = sprite.height;
+    clonedSprite.angle = sprite.angle;
+
+    return clonedSprite;
   };
 
   const destroy = () => {};
@@ -21,21 +23,18 @@ const createItem = (id, sprite, pubSub) => {
   };
 
   const onPointerOut = () => {
-    pubSub.publish('itemOut');
+    // eslint-disable-next-line no-use-before-define
+    pubSub.publish('itemOut', { item: self });
   };
 
+  // Initalization
   sprite.interactive = true;
   sprite.buttonMode = true;
 
-  sprite
-    // .on('pointerdown', onMouseDown)
-    // .on('pointerup', onMouseUp)
-    // .on('pointerupoutside', onMouseUp)
-    .on('pointerover', onPointerOver)
-    .on('pointerout', onPointerOut);
+  sprite.on('pointerover', onPointerOver).on('pointerout', onPointerOut);
 
   const self = {
-    clone,
+    cloneSprite,
     destroy,
     sprite,
     id
