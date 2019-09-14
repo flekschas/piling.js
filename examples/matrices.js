511a3e8c2373cbe7d59384fa62a12f29eade5a8b
Fritz Lekschas <service@lekschas.de>
2019-09-14 19:30:19 -0400

    Implement a webgl-based matrix renderer

---
+++ examples/matrices.js
@@ -0,0 +1,21 @@
+import { interpolateMagma } from 'd3-scale-chromatic';
+
+import createPileMe from '../src/index';
+import createMatrixRenderer from '../src/matrix-renderer';
+
+import peaks from './data/rao-2014-gm12878-chr-22-peaks.json';
+
+const hex2rgba = (hex, alpha = 1) => [
+  ...hex.match(/\w\w/g).map(x => parseInt(x, 16) / 256),
+  alpha
+];
+const numColors = 256;
+const colorMap = new Array(numColors)
+  .fill(0)
+  .map((x, i) => hex2rgba(interpolateMagma(i / numColors)));
+const matrixRenderer = createMatrixRenderer({ colorMap });
+const pileMe = createPileMe(document.getElementById('demo'));
+
+pileMe.set('renderer', matrixRenderer);
+pileMe.set('items', peaks);
+pileMe.set('grid', [10]);
---
+++ src/matrix-renderer.js
@@ -0,0 +1,147 @@
+import * as PIXI from 'pixi.js';
+import createRegl from 'regl';
+
+const VS = `
+  precision mediump float;
+
+  attribute vec2 aPosition;
+
+  // index into the texture state
+  varying vec2 vTexIdx;
+
+  void main() {
+    // map bottom left -1,-1 (normalized device coords) to 0,0 (particle texture index)
+    // and 1,1 (ndc) to 1,1 (texture)
+    vTexIdx = 0.5 * (1.0 + aPosition);
+    gl_Position = vec4(aPosition, 0, 1);
+  }
+`;
+
+const FS = `
+  precision mediump float;
+
+  // states to read from to get velocity
+  uniform sampler2D uDataTex;
+  uniform float uMinValue;
+  uniform float uMaxValue;
+  uniform sampler2D uColorMapTex;
+  uniform float uColorMapRes;
+
+  varying vec2 vTexIdx;
+
+  vec3 toColor(float value) {
+    float linIdx = (value - uMinValue) / uMaxValue * uColorMapRes * uColorMapRes;
+    vec2 texIdx = vec2(
+      (mod(linIdx, uColorMapRes) / uColorMapRes),
+      (floor(linIdx / uColorMapRes) / uColorMapRes)
+    );
+    return texture2D(uColorMapTex, texIdx).xyz;
+  }
+
+  void main() {
+    float value = texture2D(uDataTex, vTexIdx).x;
+    gl_FragColor = vec4(toColor(value), 1.0);
+  }
+`;
+
+const createColorTexture = (regl, colors) => {
+  const colorTexRes = Math.max(2, Math.ceil(Math.sqrt(colors.length)));
+  const rgba = new Float32Array(colorTexRes ** 2 * 4);
+  colors.forEach((color, i) => {
+    rgba[i * 4] = color[0]; // r
+    rgba[i * 4 + 1] = color[1]; // g
+    rgba[i * 4 + 2] = color[2]; // b
+    rgba[i * 4 + 3] = color[3]; // a
+  });
+
+  return [
+    regl.texture({
+      data: rgba,
+      shape: [colorTexRes, colorTexRes, 4],
+      type: 'float'
+    }),
+    colorTexRes
+  ];
+};
+
+const matrixRenderer = ({
+  colorMap,
+  minValue = 0,
+  maxValue = 1
+}) => sources => {
+  const canvas = document.createElement('canvas');
+
+  const regl = createRegl({
+    canvas,
+    // needed for float textures
+    extensions: 'OES_texture_float'
+  });
+
+  const textureBuffer = new Float32Array(16 ** 2 * 4);
+  const texture = regl.texture({
+    data: textureBuffer,
+    shape: [16, 16, 4],
+    type: 'float'
+  });
+
+  const framebuffer = regl.framebuffer({
+    color: texture,
+    depth: false,
+    stencil: false
+  });
+
+  const [uColorMapTex, uColorMapRes] = createColorTexture(regl, colorMap);
+
+  let dataTexture;
+
+  const renderTexture = regl({
+    framebuffer: () => framebuffer,
+
+    vert: VS,
+    frag: FS,
+
+    attributes: {
+      // a triangle big enough to fill the screen
+      aPosition: [-4, 0, 4, 4, 4, -4]
+    },
+
+    uniforms: {
+      uColorMapTex,
+      uColorMapRes,
+      uMinValue: minValue,
+      uMaxValue: maxValue,
+      // Must use a function to pick up the most current `dataTexture`
+      uDataTex: () => dataTexture
+    },
+
+    count: 3
+  });
+
+  const textures = sources.map(
+    ({ data, shape }) =>
+      new Promise(resolve => {
+        dataTexture = regl.texture({
+          data,
+          shape: [...shape, 1],
+          type: 'float'
+        });
+
+        regl.clear({
+          // background color (transparent)
+          color: [0, 0, 0, 0],
+          depth: 1
+        });
+
+        renderTexture(() => {
+          regl.draw();
+          resolve(PIXI.Texture.fromBuffer(regl.read(), ...shape));
+        });
+      })
+  );
+
+  framebuffer.destroy();
+
+  return Promise.all(textures);
+};
+
+export default matrixRenderer;
