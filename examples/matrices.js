068ee1bff5532efebf79540bd6d9a913aea767b9
eeeeenchanted <597380714@qq.com>
2019-09-17 17:39:50 -0400

    give cover renderer a new colormap

--- examples/matrices.js
+++ examples/matrices.js
@@ -16,7 +16,14 @@ const numColors = 256;
 const colorMap = new Array(numColors)
   .fill(0)
   .map((x, i) => hex2rgba(interpolateMagma(i / numColors)));
+const coverColorMap = new Array(numColors)
+  .fill(0)
+  .map((x, i) => hex2rgba(interpolateMagma((numColors - i) / numColors)));
 const matrixRenderer = createMatrixRenderer({ colorMap, shape: [16, 16] });
+const coverRenderer = createMatrixRenderer({
+  colorMap: coverColorMap,
+  shape: [16, 16]
+});
 const previewRenderer = createMatrixRenderer({ colorMap, shape: [16, 1] });
 const matrixCoverAggregator = createMatrixCoverAggregator('mean');
 const matrixPreviewAggregator = createMatrixPreviewAggregator('mean');
@@ -25,8 +32,9 @@ const pileMe = createPileMe(document.getElementById('demo'));
 pileMe.set('renderer', matrixRenderer);
 pileMe.set('itemRenderer', matrixRenderer);
 pileMe.set('previewRenderer', previewRenderer);
-pileMe.set('aggregateRenderer', matrixRenderer);
+pileMe.set('aggregateRenderer', coverRenderer);
 pileMe.set('coverAggregator', matrixCoverAggregator);
 pileMe.set('previewAggregator', matrixPreviewAggregator);
 pileMe.set('items', peaks);
 pileMe.set('grid', [10]);
+// pileMe.set('previewSpacing', 1);
