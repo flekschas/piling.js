a98954399cb63ec44488a86a1e6dc929b537032b
eeeeenchanted <597380714@qq.com>
2019-09-20 21:33:58 -0400

    change colormap

--- examples/matrices.js
+++ examples/matrices.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-duplicates */
-import { interpolateMagma } from 'd3-scale-chromatic';
+import { interpolateOrRd, interpolateRdPu } from 'd3-scale-chromatic';
 
 import createPileMe from '../src/index';
 import createMatrixRenderer from '../src/matrix-renderer';
@@ -8,17 +8,28 @@ import createMatrixPreviewAggregator from '../src/matrix-preview-aggregator';
 
 import peaks from './data/rao-2014-gm12878-chr-22-peaks.json';
 
-const hex2rgba = (hex, alpha = 1) => [
-  ...hex.match(/\w\w/g).map(x => parseInt(x, 16) / 256),
-  alpha
-];
+// const hex2rgba = (hex, alpha = 1) => [
+//   ...hex.match(/\w\w/g).map(x => parseInt(x, 16) / 256),
+//   alpha
+// ];
+const rgbStr2rgba = (rgbStr, alpha = 1) => {
+  return [
+    ...rgbStr
+      .match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/)
+      .slice(1, 4)
+      .map(x => parseInt(x, 10) / 256),
+    alpha
+  ];
+};
 const numColors = 256;
 const colorMap = new Array(numColors)
   .fill(0)
-  .map((x, i) => hex2rgba(interpolateMagma(i / numColors)));
+  .map((x, i) => rgbStr2rgba(interpolateRdPu((numColors - i) / numColors)));
+colorMap[0] = [0, 0, 0, 0];
+
 const coverColorMap = new Array(numColors)
   .fill(0)
-  .map((x, i) => hex2rgba(interpolateMagma((numColors - i) / numColors)));
+  .map((x, i) => rgbStr2rgba(interpolateOrRd((numColors - i) / numColors)));
 const matrixRenderer = createMatrixRenderer({ colorMap, shape: [16, 16] });
 const coverRenderer = createMatrixRenderer({
   colorMap: coverColorMap,
