3731a20984e936b4a76f388f916529734949fa95
Fritz Lekschas <service@lekschas.de>
2019-10-29 14:01:37 -0400

    Add helper method for parsing various color formats into decimal and alpha

--- src/utils.js
+++ src/utils.js
@@ -277,6 +277,39 @@ export const scaleLinear = () => {
   return scale;
 };
 
+export const colorToDecAlpha = (color, defaultAlpha = 1) => {
+  if (typeof color === 'string' || color instanceof String) {
+    // HEX
+    if (color[0] === '#') return [parseInt(color.substr(1), 16), defaultAlpha];
+
+    // RGBA
+    if (color.substring(0, 3) === 'rgb') {
+      const matches = color.match(/(\d+),\s*(\d+),\s*(\d+),?\s*([\d.]+)?/);
+      return [
+        matches
+          .slice(1, 3)
+          // eslint-disable-next-line no-bitwise
+          .map((x, i) => +x << (8 * (2 - i)))
+          .reduce((x, sum) => sum + x, 0),
+        +matches[4] || 1
+      ];
+    }
+
+    // RGB
+    return [
+      color
+        .match(/(\d+),\s*(\d+),\s*(\d+)/)
+        .slice(1)
+        // eslint-disable-next-line no-bitwise
+        .map((x, i) => +x << (8 * (2 - i)))
+        .reduce((x, dec) => dec + x, 0),
+      defaultAlpha
+    ];
+  }
+
+  return [parseInt(color, 10), defaultAlpha];
+};
+
 export const contextMenuTemplate =
   "<style id = 'style'>" +
   '.contextmenu {' +
