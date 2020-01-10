b16f2b179979d56ba7b4019ccc2438634721c438
eeeeenchanted <597380714@qq.com>
2020-01-10 12:35:12 +0800

    Update draw grid line #60

--- src/library.js
+++ src/library.js
@@ -1652,14 +1652,19 @@ const createPilingJs = (rootElement, initOptions = {}) => {
       canvas.getBoundingClientRect().height;
     const { width } = canvas.getBoundingClientRect();
 
+    const vLineNum = Math.ceil(width / layout.cellWidth);
+    const hLineNum = Math.ceil(height / layout.cellHeight);
+
     if (!isGridShown) {
       gridGfx.clear();
       gridGfx.lineStyle(1, 0x787878, 1);
-      for (let i = 1; i < layout.colNum + 1; i++) {
+      // vertical lines
+      for (let i = 1; i < vLineNum; i++) {
         gridGfx.moveTo(i * layout.cellWidth, 0);
         gridGfx.lineTo(i * layout.cellWidth, height);
       }
-      for (let i = 1; i < layout.rowNum + 3; i++) {
+      // horizontal lines
+      for (let i = 1; i < hLineNum; i++) {
         gridGfx.moveTo(0, i * layout.cellHeight);
         gridGfx.lineTo(width, i * layout.cellHeight);
       }
