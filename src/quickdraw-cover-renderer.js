3dbdfdf41bb2d29db25e681ba51a0fe4242911b2
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-02-18 11:13:59 -0500

    No need to use the canvas object. Pixi allows to create textures from buffers directly

--- src/quickdraw-cover-renderer.js
+++ src/quickdraw-cover-renderer.js
@@ -1,17 +1,6 @@
 import * as PIXI from 'pixi.js';
 
-const renderCanvas = (imageData, size = 64) => {
-  const canvas = document.createElement('canvas');
-  canvas.width = size;
-  canvas.height = size;
-  const ctx = canvas.getContext('2d');
-
-  ctx.putImageData(imageData, 0, 0);
-
-  return PIXI.Texture.from(canvas);
-};
-
-const createQuickDrawCoverRenderer = size => sources =>
-  Promise.all(sources.map(src => renderCanvas(src, size)));
+const createQuickDrawCoverRenderer = (size = 64) => sources =>
+  Promise.all(sources.map(src => PIXI.Texture.fromBuffer(src, size, size)));
 
 export default createQuickDrawCoverRenderer;
