41f6baeae50d99522a971235b61e745ab6efc41e
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-02-23 23:10:42 -0500

    Switch to cell-based tile extraction by clusters

--- examples/vitessce.js
+++ examples/vitessce.js
@@ -1,8 +1,10 @@
 import * as PIXI from 'pixi.js';
-import { openArray } from 'zarr';
+// From previous image tiling:
+// import { openArray } from 'zarr';
 import createPilingJs from '../src/library';
 import createVitessceRenderer from './vitessce-renderer';
 import { createUmap } from '../src/dimensionality-reducer';
+import createBBox from '../src/bounding-box';
 
 // const ZARR_URL =
 //   'https://vitessce-demo-data.storage.googleapis.com/test-data/vanderbilt-data/vanderbilt_mxif_ims.zarr/mxif_pyramid';
@@ -16,8 +18,8 @@ import { createUmap } from '../src/dimensionality-reducer';
 //   'FITC - Laminin (basement membrane)': ZARR_URL
 // };
 
-// const METADATA_URL =
-//   'https://vitessce-data.s3.amazonaws.com/0.0.20/master_release/linnarsson/linnarsson.cells.json';
+const METADATA_URL =
+  'https://vitessce-data.s3.amazonaws.com/0.0.20/master_release/linnarsson/linnarsson.cells.json';
 
 const ZARR_URL =
   'https://vitessce-data.storage.googleapis.com/linnarsson.images.zarr/pyramid';
@@ -29,71 +31,137 @@ const ZARR_CHANNELS = {
   nuclei: ZARR_URL
 };
 
-async function getZarrMetadata({
-  channels = ZARR_CHANNELS,
-  minZoom = ZARR_MIN_ZOOM
-} = {}) {
-  function range(len) {
-    return [...Array(len).keys()];
-  }
+// From previous image tiling:
+// async function getZarrMetadata({
+//   channels = ZARR_CHANNELS,
+//   minZoom = ZARR_MIN_ZOOM
+// } = {}) {
+//   function range(len) {
+//     return [...Array(len).keys()];
+//   }
 
-  const rootZarrUrl = Object.values(channels)[0]; // all are the same so get first
+//   const rootZarrUrl = Object.values(channels)[0]; // all are the same so get first
 
-  // Known issue with how zarr.js does string concatenation for urls
-  // The prefix gets chunked off for some reason and must be repeating in the config.
-  // https://github.com/gzuidhof/zarr.js/issues/36
-  const prefix = rootZarrUrl.split('/').slice(-1)[0];
+//   // Known issue with how zarr.js does string concatenation for urls
+//   // The prefix gets chunked off for some reason and must be repeating in the config.
+//   // https://github.com/gzuidhof/zarr.js/issues/36
+//   const prefix = rootZarrUrl.split('/').slice(-1)[0];
 
-  // Not necessary but this is something we should be parsing from metadata
-  const maxLevel = -minZoom;
+//   // Not necessary but this is something we should be parsing from metadata
+//   const maxLevel = -minZoom;
 
-  const zarrStores = range(maxLevel).map(i => {
-    const config = {
-      store: rootZarrUrl,
-      path: `${prefix}/${String(i).padStart(2, '0')}`,
-      mode: 'r'
-    };
-    return openArray(config);
-  });
-  const connections = await Promise.all(zarrStores);
+//   const zarrStores = range(maxLevel).map(i => {
+//     const config = {
+//       store: rootZarrUrl,
+//       path: `${prefix}/${String(i).padStart(2, '0')}`,
+//       mode: 'r'
+//     };
+//     return openArray(config);
+//   });
+//   const connections = await Promise.all(zarrStores);
 
-  // Get other properties for image viewer
+//   // Get other properties for image viewer
 
-  // Somewhat hard coded for now, but good to keep all this logic in the data loaders so we can edit in the future.
-  const baseLayer = connections[0]; // shape [4, 36040, 52660]
-  // last two dimensions of the 3D array are width and height
-  const [imageHeight, imageWidth] = baseLayer.shape.slice(1);
-  // chunks are [4, 512, 512], grab last dimentsion. Maybe add check for if last two are the same?
-  const tileSize = baseLayer.chunks.slice(-1)[0];
+//   // Somewhat hard coded for now, but good to keep all this logic in the data loaders so we can edit in the future.
+//   const baseLayer = connections[0]; // shape [4, 36040, 52660]
+//   // last two dimensions of the 3D array are width and height
+//   const [imageHeight, imageWidth] = baseLayer.shape.slice(1);
+//   // chunks are [4, 512, 512], grab last dimentsion. Maybe add check for if last two are the same?
+//   const tileSize = baseLayer.chunks.slice(-1)[0];
 
-  // Ideally we will also have metadata here about the minZoom so it's not a parameter supplied in App.js
-  return { connections, imageHeight, imageWidth, tileSize, minZoom };
-}
+//   // Ideally we will also have metadata here about the minZoom so it's not a parameter supplied in App.js
+//   return { connections, imageHeight, imageWidth, tileSize, minZoom };
+// }
 
 const createVitessce = async element => {
-  const { width: baseWidth } = element.getBoundingClientRect();
+  // From previous image tiling:
+  // const { width: baseWidth } = element.getBoundingClientRect();
+  // const { imageHeight, imageWidth } = await getZarrMetadata();
 
-  const { imageHeight, imageWidth } = await getZarrMetadata();
-  // const metadata = await fetch(METADATA_URL);
+  const response = await fetch(METADATA_URL);
+  const metadata = await response.json();
 
-  const numCols = 10;
-  const itemSize = Math.floor(baseWidth / numCols);
-  const stepSize = imageWidth / numCols;
-  const offset = stepSize / 2;
-  const numRows = Math.ceil(imageHeight / stepSize);
-  const zoomOutLevel = -Math.log2(imageWidth / baseWidth);
+  // Stratify cells by factors
+  const cellsByFactor = {};
+  Object.entries(metadata).forEach(([id, cell]) => {
+    const { cluster, subcluster } = cell.factors;
 
-  const items = new Array(numRows).fill().flatMap((_, i) =>
-    // eslint-disable-next-line no-shadow
-    new Array(numCols).fill().map((_, j) => ({
-      src: {
-        target: [offset + stepSize * j, offset + stepSize * i, 0],
-        zoom: zoomOutLevel
-      },
-      cX: offset + stepSize * j,
-      cY: offset + stepSize * i
-    }))
-  );
+    if (!cellsByFactor[cluster]) cellsByFactor[cluster] = {};
+    if (!cellsByFactor[subcluster]) cellsByFactor[subcluster] = {};
+
+    cellsByFactor[cluster][id] = cell;
+    cellsByFactor[subcluster][id] = cell;
+  }, {});
+
+  const selectedFactor = 'Microglia';
+
+  // From previous image tiling:
+  // const columns = 10;
+  // const itemSize = Math.floor(baseWidth / columns);
+  // const stepSize = imageWidth / columns;
+  // const offset = stepSize / 2;
+  // const numRows = Math.ceil(imageHeight / stepSize);
+  // const zoomOutLevel = -Math.log2(imageWidth / baseWidth);
+
+  const itemSize = 64;
+
+  const polyToBbox = polygon =>
+    createBBox()(
+      polygon.reduce(
+        (bBox, point) => ({
+          minX: Math.min(bBox.minX, point[0]),
+          minY: Math.min(bBox.minY, point[1]),
+          maxX: Math.max(bBox.maxX, point[0]),
+          maxY: Math.max(bBox.maxY, point[1])
+        }),
+        {
+          minX: Infinity,
+          minY: Infinity,
+          maxX: -Infinity,
+          maxY: -Infinity
+        }
+      )
+    );
+
+  const createItems = factor =>
+    Object.entries(cellsByFactor[factor]).map(([id, cell]) => {
+      const item = {
+        id,
+        embeddingTsne: cell.mappings['t-SNE'],
+        embeddingPca: cell.mappings.PCA
+      };
+
+      Object.entries(cell.genes).forEach(([geneName, value]) => {
+        item[`gene${geneName}`] = value;
+      });
+
+      const bBox = polyToBbox(cell.poly);
+      const target = [bBox.cX, bBox.cY];
+      const cellSize = Math.max(bBox.width, bBox.height);
+      const zoom = Math.max(ZARR_MIN_ZOOM, -Math.log2(cellSize / itemSize));
+
+      item.src = {
+        target,
+        zoom
+      };
+
+      return item;
+    });
+
+  const items = createItems(selectedFactor);
+
+  // From previous image tiling:
+  // const items = new Array(numRows).fill().flatMap((_, i) =>
+  //   // eslint-disable-next-line no-shadow
+  //   new Array(columns).fill().map((_, j) => ({
+  //     src: {
+  //       target: [offset + stepSize * j, offset + stepSize * i, 0],
+  //       zoom: zoomOutLevel
+  //     },
+  //     cX: offset + stepSize * j,
+  //     cY: offset + stepSize * i
+  //   }))
+  // );
 
   const vitessceRenderer = createVitessceRenderer(
     {
@@ -133,10 +201,9 @@ const createVitessce = async element => {
     darkMode: true,
     dimensionalityReducer: umap,
     renderer: vitessceRenderer,
-    items,
+    items: items.slice(0, 2),
     itemSize,
-    numCols,
-    cellPadding: 0,
+    cellPadding: 8,
     pileItemAlignment: false,
     pileItemRotation: true,
     pileContextMenuItems: [
@@ -148,7 +215,32 @@ const createVitessce = async element => {
     ]
   });
 
-  return piling;
+  const additionalSidebarOptions = [
+    {
+      id: 'factor',
+      title: 'Cluster',
+      fields: [
+        {
+          name: 'items',
+          dtype: 'string',
+          defaultValue: selectedFactor,
+          values: ['-', ...Object.keys(cellsByFactor)],
+          labels: [
+            '-',
+            ...Object.keys(cellsByFactor).map(
+              cluster =>
+                `${cluster} (${Object.keys(cellsByFactor[cluster]).length})`
+            )
+          ],
+          setter: factor => {
+            piling.set('items', factor);
+          }
+        }
+      ]
+    }
+  ];
+
+  return [piling, additionalSidebarOptions];
 };
 
 export default createVitessce;
