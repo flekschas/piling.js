88a98441e7572836073260ef853d8d645b30535d
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-02-28 12:14:36 -0500

    Useful for checking if the scale factor has changed

--- src/with-scale.js
+++ src/with-scale.js
@@ -3,6 +3,7 @@ import { assign } from '@flekschas/utils';
 const withScale = sprite => self =>
   assign(self, {
     scale(scaleFactor) {
+      self.scaleFactor = scaleFactor;
       sprite.width = sprite.texture.width * scaleFactor;
       sprite.height = sprite.texture.height * scaleFactor;
     }
