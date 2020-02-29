88f3ebb4bee623b4961b255cc5a8fdc32190b71c
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-02-29 15:36:26 -0500

    Add utility function to determine which transition-end event name to use

--- src/utils.js
+++ src/utils.js
@@ -152,3 +152,23 @@ export const colorToDecAlpha = (color, defaultAlpha = 1) => {
 
   return [parseInt(color, 10), defaultAlpha];
 };
+
+export const whichTransitionEvent = () => {
+  const el = document.createElement('fake-element');
+  const transitions = {
+    WebkitTransition: 'webkitTransitionEnd',
+    MozTransition: 'transitionend',
+    MSTransition: 'msTransitionEnd',
+    OTransition: 'oTransitionEnd',
+    transition: 'transitionEnd'
+  };
+
+  // eslint-disable-next-line
+  for (const t in transitions) {
+    if (el.style[t] !== undefined) {
+      return transitions[t];
+    }
+  }
+
+  return 'transitionEnd';
+};
