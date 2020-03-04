937056d7aa46177d766cabf11edb044dd0b5e961
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-03-03 22:43:10 -0500

    No need for a worker here

--- examples/scatterplot-cover-aggregator.js
+++ examples/scatterplot-cover-aggregator.js
@@ -1,19 +1,6 @@
-import { createWorker } from '@flekschas/utils';
-import workerFn from './scatterplot-cover-aggregator-worker';
+import { identity } from '@flekschas/utils';
 
 const createScatterplotCoverAggregator = () => sources =>
-  new Promise((resolve, reject) => {
-    const worker = createWorker(workerFn);
-
-    worker.onmessage = e => {
-      if (e.data.error) reject(e.data.error);
-      else resolve(e.data.newSrc);
-      worker.terminate();
-    };
-
-    worker.postMessage({
-      sources
-    });
-  });
+  Promise.resolve(sources.flatMap(identity));
 
 export default createScatterplotCoverAggregator;
