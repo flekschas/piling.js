0f7c8e0c4591914e0b63d3d7b1340f4bf87f54d0
eeeeenchanted <597380714@qq.com>
2020-03-07 12:56:30 +0800

    Update time-curve.js

--- examples/time-curve.js
+++ examples/time-curve.js
@@ -1,44 +1,11 @@
-import * as d3 from 'd3';
 import createPilingJs from '../src/library';
 import { createImageRenderer } from '../src/renderer';
 
 const createTimeCurvePiles = async element => {
-  const svg = await d3
-    .xml('data/cloud-curve.svg')
-    .then(data => data.documentElement);
-
-  const circle = d3.select(svg).selectAll('.point');
-
-  const width = d3
-    .select(svg)
-    .select('rect')
-    .attr('width');
-  const height = d3
-    .select(svg)
-    .select('rect')
-    .attr('height');
-
-  const x = [];
-  const y = [];
-
-  // eslint-disable-next-line no-underscore-dangle
-  circle._groups[0].forEach(c => {
-    const u = c.cx.baseVal.value / width;
-    const v = c.cy.baseVal.value / height;
-
-    x.push(u);
-    y.push(v);
-  });
-
-  const imageRenderer = createImageRenderer();
-
-  const response = await fetch('data/cloud.json');
+  const response = await fetch('data/time-curve.json');
   const data = await response.json();
 
-  data.forEach((item, i) => {
-    item.u = x[i];
-    item.v = y[i];
-  });
+  const imageRenderer = createImageRenderer();
 
   const piling = createPilingJs(element, {
     renderer: imageRenderer,
@@ -47,7 +14,10 @@ const createTimeCurvePiles = async element => {
     darkMode: true
   });
 
-  piling.arrangeBy('data', ['u', 'v']);
+  piling.arrangeBy('data', {
+    property: item => item.mds,
+    propertyIsVector: true
+  });
 
   return [piling];
 };
