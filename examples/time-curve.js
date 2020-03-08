0d172f4127f3872ed3b462ba1f5b39441d2dbaaf
eeeeenchanted <597380714@qq.com>
2020-03-08 17:38:47 +0800

    Draw pile connections; add sidebar option

--- examples/time-curve.js
+++ examples/time-curve.js
@@ -1,3 +1,4 @@
+import * as d3 from 'd3';
 import createPilingJs from '../src/library';
 import { createImageRenderer } from '../src/renderer';
 
@@ -7,6 +8,58 @@ const createTimeCurvePiles = async element => {
 
   const imageRenderer = createImageRenderer();
 
+  const { width, height } = element.getBoundingClientRect();
+
+  const drawPileConnections = prop => {
+    d3.select('#connection').remove();
+
+    const svg = d3
+      .select(element)
+      .append('svg')
+      .attr('id', 'connection')
+      .attr('viewBox', `0 0 ${width} ${height}`);
+
+    const linesBetweenFrames = data.map((frame, index) =>
+      index === data.length - 1 ? [frame] : [frame, data[index + 1]]
+    );
+
+    const line = d3
+      .line()
+      .x(d => d[prop][0] * width)
+      .y(d => d[prop][1] * height);
+
+    const gradient = d3.interpolate('white', 'red');
+
+    const numOfLines = linesBetweenFrames.length;
+
+    linesBetweenFrames.forEach((lineData, index) => {
+      svg
+        .append('path')
+        .attr('d', line(lineData))
+        .attr('stroke', gradient(index / numOfLines))
+        .attr('stroke-width', 3);
+    });
+  };
+
+  const additionalSidebarOptions = [
+    {
+      id: 'positionby',
+      title: 'Position By',
+      fields: [
+        {
+          name: 'algorithm',
+          dtype: 'string',
+          defaultValue: 'umap',
+          values: ['umap', 'mds', 'tsne'],
+          setter: values => {
+            piling.arrangeBy('uv', pile => data[pile.items[0]][values]);
+            drawPileConnections(values);
+          }
+        }
+      ]
+    }
+  ];
+
   const piling = createPilingJs(element, {
     renderer: imageRenderer,
     items: data,
@@ -14,16 +67,11 @@ const createTimeCurvePiles = async element => {
     darkMode: true
   });
 
-  piling.arrangeBy('data', [
-    {
-      property: item => item.umap[0]
-    },
-    {
-      property: item => item.umap[1]
-    }
-  ]);
+  piling.arrangeBy('uv', pile => data[pile.items[0]].umap);
 
-  return [piling];
+  drawPileConnections('umap');
+
+  return [piling, additionalSidebarOptions];
 };
 
 export default createTimeCurvePiles;
