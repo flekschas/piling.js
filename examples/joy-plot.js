4c61ac40282cc92832c9f06ed5e599869c19e107
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-03-09 10:37:31 -0400

    Update to data from NASA

--- examples/joy-plot.js
+++ examples/joy-plot.js
@@ -3,7 +3,7 @@ import createPilingJs from '../src/library';
 import { createSvgRenderer } from '../src/renderer';
 
 const createSvgLinesPiles = async element => {
-  const response = await fetch('data/monthly_density_avg_temp.json');
+  const response = await fetch('data/monthly_temp_deviation_decades.json');
   const data = await response.json();
 
   const { width } = element.getBoundingClientRect();
@@ -24,8 +24,31 @@ const createSvgLinesPiles = async element => {
   const barWidth = 100 / numBins;
   const barHalf = barWidth / 2;
 
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
+
   const createSvgStart = () =>
     `<svg viewBox="0 0 100 ${absHeight}" xmlns="http://www.w3.org/2000/svg">`;
+
+  const createGradient = (name, startColor, midColor, endColor) => `<defs>
+  <linearGradient id="${name}" x1="0%" y1="0%" x2="100%" y2="0%">
+    <stop offset="0%"   stop-color="${startColor}"/>
+    <stop offset="50%"  stop-color="${midColor}"/>
+    <stop offset="100%" stop-color="${endColor}"/>
+  </linearGradient>
+</defs>`;
 
   const createSvgEnd = () => '</svg>';
 
@@ -33,34 +56,46 @@ const createSvgLinesPiles = async element => {
     .x((_, i) => barHalf + barWidth * i)
     .y(d => absHeight - absHeight * d);
 
-  const createPath = (
-    kde,
-    { stroke = 'black', fill = 'currentColor' } = {}
-  ) => {
+  const createPath = kde => {
     const path = createLine(kde);
-    return `<path d="M${absHeight},0${path}M${absHeight},${absHeight}L" stroke="${stroke}" stroke-size="1" fill="${fill}"/>`;
+    return `<path d="M${absHeight},0${path}L100,${absHeight}L" stroke="url(#linear-stroke)" stroke-size="1" fill="url(#linear-fill)"/>`;
   };
 
+  const createTitle = (titleLeft, titleRight) =>
+    `<foreignObject x="0" y="0" width="100" height="8">
+  <div xmlns="http://www.w3.org/1999/xhtml" style="display: flex; justify-content: space-between; font: 8px sans-serif; color: #808080">
+    <span>${titleLeft}</span>
+    <span>${titleRight}</span>
+  </div>
+</foreignObject>`;
+
   // prettier-ignore
-  const createLinePlot = (hist) => [
+  const createLinePlot = (hist, years, month) => [
     createSvgStart(),
+    createGradient('linear-fill', '#3170ad', '#cccccc', '#c76526'),
+    createGradient('linear-stroke', '#1d4266', '#666666', '#663413'),
+    createTitle(months[month], years),
     createPath(hist),
     createSvgEnd()
   ].join('');
 
-  const startYear = 1948;
-  const numYearPerStep = 5;
+  const startYear = 1880;
+  const numYearPerStep = 10;
 
-  const items = data.flatMap((kdes, month) => {
-    return kdes.map((kde, halfDecade) => ({
-      kde,
-      src: createLinePlot(kde),
-      years: `From ${startYear + numYearPerStep * halfDecade}-${startYear +
-        numYearPerStep * (halfDecade + 1)}`,
-      month,
-      halfDecade
-    }));
-  });
+  const items = data.flatMap((kdes, month) =>
+    kdes.map((kde, decade) => {
+      const years = `${startYear + numYearPerStep * decade}-${startYear +
+        numYearPerStep * (decade + 1) -
+        1}`;
+      return {
+        kde,
+        src: createLinePlot(kde, years, month),
+        years,
+        month,
+        decade
+      };
+    })
+  );
 
   const piling = createPilingJs(element, {
     cellAspectRatio: aspectRatio,
@@ -78,6 +113,8 @@ const createSvgLinesPiles = async element => {
     lassoFillColor: '#000000',
     lassoStrokeColor: '#000000'
   });
+
+  piling.arrangeBy('data', 'decade');
 
   return piling;
 };
