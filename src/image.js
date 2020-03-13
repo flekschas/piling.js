6ba40661965604231e691e8b895f08bdb9b9ad78
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-03-13 15:20:13 -0400

    Add support for cloning

--- src/image.js
+++ src/image.js
@@ -1,6 +1,7 @@
 import { pipe, withConstructor, withStaticProperty } from '@flekschas/utils';
 import * as PIXI from 'pixi.js';
 
+import withClone from './with-clone';
 import withColorFilters from './with-color-filters';
 import withDestroy from './with-destroy';
 import withScale from './with-scale';
@@ -17,13 +18,14 @@ const createImage = (texture, { anchor = [0.5, 0.5] } = {}) => {
   }
 
   return pipe(
+    withConstructor(createImage),
     withStaticProperty('displayObject', sprite),
     withStaticProperty('sprite', sprite),
+    withClone(texture, { anchor }),
     withColorFilters(sprite),
     withScale(sprite, texture.width, texture.height),
     withSize(sprite, texture.width, texture.height),
-    withDestroy(sprite),
-    withConstructor(createImage)
+    withDestroy(sprite)
   )({});
 };
 
