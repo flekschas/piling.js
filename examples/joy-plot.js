82de35c956fa8837d54e6ba45ae823d756db5e63
eeeeenchanted <597380714@qq.com>
2020-03-14 14:27:42 +0800

    Add item label

--- examples/joy-plot.js
+++ examples/joy-plot.js
@@ -129,7 +129,8 @@ const createSvgLinesPiles = async element => {
     pileScale: pile => 1 + Math.min(0.5, (pile.items.length - 1) * 0.1),
     backgroundColor: '#ffffff',
     lassoFillColor: '#000000',
-    lassoStrokeColor: '#000000'
+    lassoStrokeColor: '#000000',
+    itemLabel: ['decade']
   });
 
   piling.arrangeBy('data', 'numDecade');
