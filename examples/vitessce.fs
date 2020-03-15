a5943ac265f3bdbc9a92b914ae1fbb2d3cb06df1
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-03-15 12:26:22 -0400

    Adjust styles to white and dark mode

--- examples/vitessce.fs
+++ examples/vitessce.fs
@@ -6,6 +6,8 @@ uniform sampler2D uTexSampler0;
 uniform sampler2D uTexSampler1;
 uniform vec3 uColors[2];
 uniform vec2 uDomains[2];
+uniform float uBackground;
+uniform float uComboModifier;
 
 in vec2 vTextureCoord;
 
@@ -21,14 +23,14 @@ void main() {
   float colorValue0 = (texture(uTexSampler0, vTextureCoord).r - uDomains[0][0]) / uDomains[0][1];
   float colorValue1 = (texture(uTexSampler1, vTextureCoord).r - uDomains[1][0]) / uDomains[1][1];
 
-  vec3 rgbCombo = vec3(0.0);
+  vec3 rgbCombo = vec3(uBackground);
   vec3 hsvCombo = vec3(0.0);
 
   float colorValues[2] = float[2](colorValue0, colorValue1);
 
   for(int i = 0; i < 2; i++) {
     hsvCombo = vec3(uColors[i].xy, max(0.0, colorValues[i]));
-    rgbCombo += hsv2rgb(hsvCombo);
+    rgbCombo += hsv2rgb(hsvCombo) * uComboModifier;
   }
 
   outColor = vec4(rgbCombo, 1.0);
