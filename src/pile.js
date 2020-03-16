87c598c4fff6691021e5ddb3c5be50ca9027f4f3
eeeeenchanted <597380714@qq.com>
2020-03-16 12:39:19 +0800

    Add placeholder graphics to content graphics; fix #149

--- src/pile.js
+++ src/pile.js
@@ -980,9 +980,9 @@ const createPile = (
   const setItems = (
     items,
     { asPreview = false } = {},
-    shouldDrawPlaceholer = false
+    shouldDrawPlaceholder = false
   ) => {
-    if (shouldDrawPlaceholer) drawPlaceholder();
+    if (shouldDrawPlaceholder) drawPlaceholder();
 
     const outdatedItems = mergeMaps(normalItemIndex, previewItemIndex);
 
@@ -1038,11 +1038,17 @@ const createPile = (
     setCover(newCover);
   };
 
-  const placeholderGfx = new PIXI.Graphics();
+  let placeholderGfx;
   let isPlaceholderDrawn = false;
 
   const drawPlaceholder = () => {
-    const { width, height } = anchorBox;
+    if (!placeholderGfx) {
+      placeholderGfx = new PIXI.Graphics();
+      contentGraphics.addChild(placeholderGfx);
+    }
+    const width = anchorBox.width / baseScale;
+    const height = anchorBox.height / baseScale;
+
     const r = width / 12;
     const color = store.state.darkMode ? 0xffffff : 0x000000;
 
@@ -1074,7 +1080,6 @@ const createPile = (
   const init = () => {
     rootGraphics.addChild(borderGraphics);
     rootGraphics.addChild(contentGraphics);
-    rootGraphics.addChild(placeholderGfx);
 
     contentGraphics.addChild(normalItemContainer);
     contentGraphics.addChild(previewItemContainer);
