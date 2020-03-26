290c62af9b541169e7c25e6cbb34372da355843c
eeeeenchanted <597380714@qq.com>
2020-03-26 14:23:18 +0800

    Add pileLabelSizeAggregator

--- src/store.js
+++ src/store.js
@@ -390,6 +390,9 @@ const [pileLabelFontSize, setPileLabelFontSize] = setter(
   7
 );
 const [pileLabelHeight, setPileLabelHeight] = setter('pileLabelHeight', 2);
+const [pileLabelSizeAggregator, setPileLabelSizeAggregator] = setter(
+  'pileLabelSizeAggregator'
+);
 
 const items = (previousState = {}, action) => {
   switch (action.type) {
@@ -640,6 +643,7 @@ const createStore = () => {
     pileLabelFontSize,
     pileLabelHeight,
     pileLabelStackAlign,
+    pileLabelSizeAggregator,
     pileLabelText,
     pileOpacity,
     piles,
@@ -804,6 +808,7 @@ export const createAction = {
   setPileLabelFontSize,
   setPileLabelHeight,
   setPileLabelStackAlign,
+  setPileLabelSizeAggregator,
   setPileLabelText,
   setPileVisibilityItems,
   setPileOpacity,
