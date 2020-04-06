b4048bda03c444d9952073aad6a8a7b66dcc06cd
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-04-06 11:44:16 -0400

    Copy all example data

--- copy-example-data.sh
+++ copy-example-data.sh
@@ -2,11 +2,31 @@
 
 mkdir -p examples-build/data
 
-cp -r examples/data/photos examples-build/data/
-cp -r examples/data/photos.json examples-build/data/
+# 1. Photos
+cp -r examples/data/coco-cars examples-build/data/
+cp -r examples/data/coco-cars.json examples-build/data/
+
+# 2. Matrices
 cp -r examples/data/rao-2014-gm12878-chr-22-peaks.json examples-build/data/
-cp -r examples/data/apple.ndjson examples-build/data/
+
+# 4. Drawings
+cp -r examples/data/cake.json examples-build/data/
+cp -r examples/data/flower.json examples-build/data/
+cp -r examples/data/neckless.json examples-build/data/
+cp -r examples/data/piano.json examples-build/data/
+cp -r examples/data/power-outlet.json examples-build/data/
+cp -r examples/data/teapot.json examples-build/data/
+cp -r examples/data/smiley-face.json examples-build/data/
+cp -r examples/data/snowman.json examples-build/data/
+
+# 5. Ridge Plot
 cp -r examples/data/monthly_temp_deviation_decades.json examples-build/data/
+
+# 7. Scatterplots
+cp -r examples/data/worldbank.json examples-build/data/
+
+# 8. Time Series
+cp -r examples/data/us-daily-precipitation.json examples-build/data/
 
 mkdir -p examples-build/docs
 
