fb35cecfcaa39b853e8aa0b3803086cbc262c23c
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-04-11 21:11:39 -0400

    Optimize animate() by calling onDone partially on idle and calling the rest upon completion of the animation

--- src/animator.js
+++ src/animator.js
@@ -1,57 +1,74 @@
 import withRaf from 'with-raf';
 
+const PARTIAL_ON_DONE_BATCH_SIZE = 500;
+
 /**
  * Factory function to create an animator
  * @param {function} render - Render funtion
  * @param {function} pubSub - PubSub messenger
  */
 const createAnimator = (render, pubSub) => {
-  const currentTweeners = new Set();
+  const activeTweeners = new Set();
+  let doneTweeners = [];
   let isAnimating = false;
 
   const onCall = () => {
-    if (currentTweeners.size) {
+    if (activeTweeners.size) {
       // eslint-disable-next-line no-use-before-define
       animateRaf();
     } else {
+      onDone();
       if (isAnimating) pubSub.publish('animationEnd');
       isAnimating = false;
     }
   };
 
+  const onDone = () => {
+    doneTweeners.forEach(tweener => tweener.onDone());
+    doneTweeners = [];
+  };
+
+  const onDonePartial = () => {
+    const tobeInvoked = doneTweeners.slice(0, PARTIAL_ON_DONE_BATCH_SIZE);
+    doneTweeners.splice(0, PARTIAL_ON_DONE_BATCH_SIZE);
+
+    window.requestIdleCallback(() => {
+      tobeInvoked.forEach(tweener => tweener.onDone());
+    });
+  };
+
   const animate = () => {
     isAnimating = true;
-    const done = [];
-    currentTweeners.forEach(tweener => {
-      if (tweener.update()) done.push(tweener);
+    activeTweeners.forEach(tweener => {
+      if (tweener.update()) doneTweeners.push(tweener);
     });
     render();
 
     // Remove tweeners that are done updating
-    done.forEach(tweener => {
-      tweener.onDone();
-      currentTweeners.delete(tweener);
-    });
+    doneTweeners.forEach(tweener => activeTweeners.delete(tweener));
+
+    // Partially invoke onDone();
+    onDonePartial();
   };
 
   const animateRaf = withRaf(animate, onCall);
 
   const add = tweener => {
-    currentTweeners.add(tweener);
+    activeTweeners.add(tweener);
     tweener.register();
     animateRaf();
   };
 
   const addBatch = tweeners => {
     tweeners.forEach(tweener => {
-      currentTweeners.add(tweener);
+      activeTweeners.add(tweener);
       tweener.register();
     });
     animateRaf();
   };
 
   const cancel = tweener => {
-    currentTweeners.delete(tweener);
+    activeTweeners.delete(tweener);
   };
 
   return {
