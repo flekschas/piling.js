d8751ba4e6d1559d309ca2702257db65c149e8f1
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-04-14 21:02:50 -0400

    Image border using a sprite rather than a graphics object

--- src/image.js
+++ src/image.js
@@ -10,24 +10,34 @@ import withSize from './with-size';
 import { toDisplayObject } from './utils';
 
 const createImage = (source, { anchor = [0.5, 0.5] } = {}) => {
+  const container = new PIXI.Container();
   const displayObject = toDisplayObject(source);
   let sprite;
 
   if (displayObject instanceof PIXI.Texture) {
+    const border = new PIXI.Sprite(PIXI.Texture.WHITE);
+    border.width = displayObject.width + 6;
+    border.height = displayObject.height + 6;
+    border.anchor.set(...anchor);
+
     sprite = new PIXI.Sprite(displayObject);
     sprite.anchor.set(...anchor);
+
+    container.addChild(border);
+    container.addChild(sprite);
   } else {
     sprite = displayObject;
+    container.addChild(sprite);
   }
 
   return pipe(
     withConstructor(createImage),
-    withStaticProperty('displayObject', sprite),
+    withStaticProperty('displayObject', container),
     withStaticProperty('sprite', sprite),
-    withClone(displayObject, { anchor }),
+    withClone(sprite, { anchor }),
     withColorFilters(sprite),
-    withScale(sprite, displayObject.width, displayObject.height),
-    withSize(sprite, displayObject.width, displayObject.height),
+    withScale(container, displayObject.width, displayObject.height),
+    withSize(container, displayObject.width, displayObject.height),
     withDestroy(sprite)
   )({});
 };
