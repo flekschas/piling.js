c4cd1ea87afeeb806d60e62c7c16bc52be6c70af
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-05-04 10:49:04 -0400

    Copy COVID and vitessce data as well

--- copy-example-data.sh
+++ copy-example-data.sh
@@ -8,6 +8,9 @@ cp -r examples/data/coco-cars-remote-subsample.json examples-build/data/coco-car
 # 2. Matrices
 cp -r examples/data/rao-2014-gm12878-chr-22-peaks.json examples-build/data/
 
+# 3. COVID-19
+cp -r examples/data/covid-19.json examples-build/data/
+
 # 4. Drawings
 cp -r examples/data/cake.json examples-build/data/
 cp -r examples/data/flower.json examples-build/data/
@@ -20,6 +23,9 @@ cp -r examples/data/snowman.json examples-build/data/
 
 # 5. Ridge Plot
 cp -r examples/data/monthly_temp_deviation_decades.json examples-build/data/
+
+# 6. Vitessce
+cp -r examples/data/vitessce-sample.json examples-build/data/
 
 # 7. Scatterplots
 cp -r examples/data/worldbank.json examples-build/data/
